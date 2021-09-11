dabf7e0fc71a03c5452e0d6e05d458d09be5717b
kosiken <allisonkosy@gmail.com>
2021-09-11 14:30:42 +0100

    first commit

---
+++ src/main/java/com/allisonkosy/App.java
@@ -0,0 +1,17 @@
+package com.allisonkosy;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App {
+    private static final Logger logger = LogManager.getLogger(App.class);
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello, user!" );
+        logger.trace("We've just greeted the user!");
+        logger.debug("We've just greeted the user!");
+        logger.info("We've just greeted the user!");
+        logger.warn("We've just greeted the user!");
+        logger.error("We've just greeted the user!");
+        logger.fatal("We've just greeted the user!");
+    }
+}
